18287d0a34ed0381565a8789b620f7be4ed673b1
gangassault <gangassault@gmail.com>
2016-09-19 16:12:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,10 +71,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/;articleName' , function (req, res){
-    //articleName == article-one
-    //articles[articleName] == {} content object for article one
-  res.send(createTemplate(articles[articleName]));
+app.get('/;articleone' , function (req, res){
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));   
 });
 
 app.get('/article-two' , function (req, res){
