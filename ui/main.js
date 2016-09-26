ff55c7fb029e9d04c97111369e3e02e4f8a63f5b
gangassault <gangassault@gmail.com>
2016-09-26 20:46:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,2 @@
 console.log('Loaded!');
 
-//Change the text of the main-text 
-var element = document.getElementById ('main-text');
-
-
-element.innerHtml = 'Gangassault'
-
-
-var img = document.getElementById ('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onClick = function (){
-    var interval = setInterval(moveRight, 100);
-};
